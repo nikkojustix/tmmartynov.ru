1027f4a87b50ccb5734739f9e2653564eda4125b
Nikolay Kotov <nickolaykotov@gmail.com>
2023-04-26 09:55:13 +0300

    testimonials

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
 import * as flsFunctions from './modules/functions.js';
+import { Splide } from '@splidejs/splide';
 
 flsFunctions.isWebp();
 
 const burgerBtn = document.querySelector('.burger__btn');
 burgerBtn.addEventListener('click', flsFunctions.toggleMobileMenu);
+
+new Splide('.testimonials', {
+  perPage: 3,
+  perMove: 1,
+  pagination: false,
+  gap: 20,
+  type: 'loop',
+  breakpoints: {
+    999: {
+      perPage: 2,
+    },
+    767: {
+      perPage: 1,
+    },
+  },
+}).mount();
